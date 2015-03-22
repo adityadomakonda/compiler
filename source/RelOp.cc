d6c4ca13657e3b09fefc46d05905632079f68429
adityadomakonda <adityadomakonda@gmail.com>
2015-03-21 22:50:31 -0400

    Basic thread creation in SelectFile class

--- source/RelOp.cc
+++ source/RelOp.cc
@@ -1,12 +1,112 @@
 #include "RelOp.h"
 
+void *Run_SelectFile(void *arg){
+	cout<<"Run_SelectFile"<<endl;
+}
+
 void SelectFile::Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal) {
+
+    thread_args_SelectFile t_args = {&inFile, &outPipe, &selOp, &literal};
+    pthread_create(&thread,NULL,Run_SelectFile,(void *)&t_args);
+    return;
 }
 
 void SelectFile::WaitUntilDone () {
-	// pthread_join (thread, NULL);
+	pthread_join (thread, NULL);
+	return;
 }
 
 void SelectFile::Use_n_Pages (int runlen) {
+
+}
+
+void SelectPipe::Run (Pipe &inPipe, Pipe &outPipe, CNF &selOp, Record &literal) { 
+
+}
+
+void SelectPipe::WaitUntilDone () { 
+	pthread_join (thread, NULL);
+	return;
+}
+
+void SelectPipe::Use_n_Pages (int n) { 
+
+}
+
+void Project::Run (Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput) { 
+
+}
+	
+void Project::WaitUntilDone () { 
+	pthread_join (thread, NULL);
+	return;
+}
+
+void Project::Use_n_Pages (int n) { 
+
+}
+
+void Join::Run (Pipe &inPipeL, Pipe &inPipeR, Pipe &outPipe, CNF &selOp, Record &literal) { 
+
+}
+
+void Join::WaitUntilDone () { 
+	pthread_join (thread, NULL);
+	return;
+}
+
+void Join::Use_n_Pages (int n) { 
+
+}
+
+void DuplicateRemoval::Run (Pipe &inPipe, Pipe &outPipe, Schema &mySchema) { 
+
+}
+
+void DuplicateRemoval::WaitUntilDone () { 
+	pthread_join (thread, NULL);
+	return;
+}
+
+void DuplicateRemoval::Use_n_Pages (int n) { 
+
+}
+
+void Sum::Run (Pipe &inPipe, Pipe &outPipe, Function &computeMe) { 
+
+}
+
+void Sum::WaitUntilDone () { 
+	pthread_join (thread, NULL);
+	return;
+}
+
+void Sum::Use_n_Pages (int n) { 
+
+}
+
+void GroupBy::Run (Pipe &inPipe, Pipe &outPipe, OrderMaker &groupAtts, Function &computeMe) { 
+
+}
+
+void GroupBy::WaitUntilDone () { 
+	pthread_join (thread, NULL);
+	return;
+}
+
+void Use_n_Pages (int n) { 
+
+}
+
+void WriteOut::Run (Pipe &inPipe, FILE *outFile, Schema &mySchema) { 
+
+}
+
+void WriteOut::WaitUntilDone () { 
+	pthread_join (thread, NULL);
+	return;
+}
+
+void WriteOut::Use_n_Pages (int n) { 
 
 }
--- source/RelOp.h
+++ source/RelOp.h
@@ -19,7 +19,7 @@ class RelationalOp {
 class SelectFile : public RelationalOp { 
 
 	private:
-	// pthread_t thread;
+	pthread_t thread;
 	// Record *buffer;
 
 	public:
@@ -30,43 +30,71 @@ class SelectFile : public RelationalOp {
 
 };
 
+typedef struct {
+	DBFile inFile;
+	Pipe outPipe;
+	CNF selOp; 
+	Record literal;
+}thread_args_selectFile;
+
 class SelectPipe : public RelationalOp {
+	private:
+	pthread_t thread;
+		
 	public:
 	void Run (Pipe &inPipe, Pipe &outPipe, CNF &selOp, Record &literal) { }
 	void WaitUntilDone () { }
 	void Use_n_Pages (int n) { }
 };
 class Project : public RelationalOp { 
+	private:
+	pthread_t thread;
+	
 	public:
 	void Run (Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput) { }
 	void WaitUntilDone () { }
 	void Use_n_Pages (int n) { }
 };
 class Join : public RelationalOp { 
+	private:
+	pthread_t thread;
+		
 	public:
 	void Run (Pipe &inPipeL, Pipe &inPipeR, Pipe &outPipe, CNF &selOp, Record &literal) { }
 	void WaitUntilDone () { }
 	void Use_n_Pages (int n) { }
 };
 class DuplicateRemoval : public RelationalOp {
+	private:
+	pthread_t thread;
+		
 	public:
 	void Run (Pipe &inPipe, Pipe &outPipe, Schema &mySchema) { }
 	void WaitUntilDone () { }
 	void Use_n_Pages (int n) { }
 };
 class Sum : public RelationalOp {
+	private:
+	pthread_t thread;
+		
 	public:
 	void Run (Pipe &inPipe, Pipe &outPipe, Function &computeMe) { }
 	void WaitUntilDone () { }
 	void Use_n_Pages (int n) { }
 };
 class GroupBy : public RelationalOp {
+	private:
+	pthread_t thread;
+		
 	public:
 	void Run (Pipe &inPipe, Pipe &outPipe, OrderMaker &groupAtts, Function &computeMe) { }
 	void WaitUntilDone () { }
 	void Use_n_Pages (int n) { }
 };
 class WriteOut : public RelationalOp {
+	private:
+	pthread_t thread;
+		
 	public:
 	void Run (Pipe &inPipe, FILE *outFile, Schema &mySchema) { }
 	void WaitUntilDone () { }
